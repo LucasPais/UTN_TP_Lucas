3e388f6b74c12b458d8e77707d010fc16f0aafdf
Lucas Pais <lucasleon466@gmail.com>
2020-09-10 16:03:51 -0300

    TP1

---
+++ TP1/Funciones.h
@@ -0,0 +1,6 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+
--- TP1/main.c
+++ TP1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
     int numeroUno;
@@ -12,31 +13,79 @@ int main()
     char multiplicacion;
     char factorizacion;
     char respuestaUno;
+    int cero=0;
 
     printf("Ingrese un numero: ");
     scanf("%d",&numeroUno);
 
-    printf("\n¿Que desea hacer acontinuacion?" "\n a)Ingresar otro numero " "\n b)Salir " "\n ");
+    printf("Ingrese otro numero: ");
+    scanf("%d",&numeroDos);
+
+
+    printf("\n ¿Que operacion desea realizar? " "\n a)Suma" "\n b)Resta" "\n c)Multiplicacion" "\n d)Divicion" "\n e)Factorizar" "\n");
     fflush(stdin);
     scanf("%c",&respuestaUno);
-
-    while(respuestaUno !='a' && respuestaUno !='b')
+    while(respuestaUno!='a' && respuestaUno!='b' && respuestaUno!='c' && respuestaUno!='d' && respuestaUno!='e')
     {
-        printf("\n ERROR.Reingrese que operacion desea hacer ""\n a)Ingresar otro numero " "\n b)Salir " "\n ");
+        printf("ERROR. Ingrese segun corresponda " "\n a)Suma" "\n b)Resta" "\n c)Multiplicacion" "\n d)Divicion" "\n e)Factorizar" "\n");
         fflush(stdin);
-        scanf("%c",&respuestaUno);
+      scanf("%c",&respuestaUno);
     }
-
     if(respuestaUno=='a')
     {
-        printf("Ingrese otro numero: ");
-        scanf("%d",&numeroDos);
+       resultado=(float)numeroUno + numeroDos;
+       printf("El resultado de la suma de %d + %d es: %d",numeroUno,numeroDos,resultado);
+
+    }else
+    {
+    if(respuestaUno=='b')
+       {
+           resultado=(float)numeroUno-numeroDos;
+           printf("El resultado de la resta de %d - %d es: %d",numeroUno,numeroDos,resultado);
+       }else
+       {
+        if(respuestaUno=='c')
+        {
+        resultado=numeroUno*numeroDos;
+        printf("El resultado de la multiplicacion de %d * %d es: %d",numeroUno,numeroDos,resultado);
+        }else
+        {
+        if(respuestaUno=='d')
+        {
+        resultado=(float)numeroUno/numeroDos;
+        printf("El resultado de la divicion de %d / %d es: %d",numeroUno,numeroDos,resultado);
+        }
+       }
+    }
+
+    }
+
+    if(respuestaUno=='e')
+    {
+
     }
 
 
 
-    printf("\n El resultado es %d:" ,resultado);
 
+/*switch(resultado){
+    case'a':
+printf("\n El resultado de la suma es: %d" ,resultado);
+    break;
+    case'b':
+        printf("\n El resultado de la resta es: %d",resultado);
+        break;
+    case'c':
+            printf("\n El resultado de la multiplicacion es: %d",resultado);
+            break;
+    case'd':
+        printf("\n El resultado de la divicion es: %d",resultado);
+        break;
+    case'e':
+        printf("\n El resultado de la factorizacion es: %d",resultado);
+        break;
+
+    }*/
 
 
 }
